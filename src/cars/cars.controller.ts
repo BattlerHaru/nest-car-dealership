12f1e290e47e1b10e4c56c7ac8b913afbbbacfe1
BattlerHaru <kronk.sdance@gmail.com>
2024-04-29 16:36:33 -0600

    :heavy_plus_sign: add libs class-validator adn class-transformer to DTO module

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -19,8 +20,9 @@ export class CarsController {
     }
 
     @Post()
-    createCar(@Body() body: any) {
-        return { message: body }
+    @UsePipes(ValidationPipe)
+    createCar(@Body() createCarDto: CreateCarDto) {
+        return { message: createCarDto }
     }
 
     @Patch(":id")
