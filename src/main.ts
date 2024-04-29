971e9713e43f05825be2f6c57748da581585a28f
BattlerHaru <kronk.sdance@gmail.com>
2024-04-29 16:43:26 -0600

    :building_construction: move Pipe to lvl global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // lvl global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // solo admite la data necesaria
+      forbidNonWhitelisted: true // muestra errores en data no necesaria
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
